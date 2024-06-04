fedcb7f42b159e0c9c74ecaf2e68066a046afe61
Farasat Khan <farasat.khan@jintech.com>
2024-06-04 16:39:20 +0500

    added simple bitcoin api

---
+++ DemoShellNavigation/DemoShellNavigation/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DemoShellNavigation.Models;
+using DemoShellNavigation.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DemoShellNavigation.ViewModels
+{
+    public class HomePageViewModel
+    {
+
+        public HomePageViewModel()
+        {
+
+        }
+
+    }
+}
--- DemoShellNavigation/DemoShellNavigation/Views/dashboard/HomePage.xaml.cs
+++ DemoShellNavigation/DemoShellNavigation/Views/dashboard/HomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DemoShellNavigation.Services;
+using DemoShellNavigation.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,20 @@ namespace DemoShellNavigation.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        private readonly BitcoinPriceService _bitcoinPriceService = new BitcoinPriceService();
         public HomePage()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            var bitcoinPriceIndex = await _bitcoinPriceService.GetBitcoinPriceIndexAsync();
+
+            UsdLabel.Text = $"USD: {bitcoinPriceIndex.bpi.USD.rate}";
+            GbpLabel.Text = $"GBP: {bitcoinPriceIndex.bpi.GBP.rate}";
+            EurLabel.Text = $"EUR: {bitcoinPriceIndex.bpi.EUR.rate}";
         }
     }
 }

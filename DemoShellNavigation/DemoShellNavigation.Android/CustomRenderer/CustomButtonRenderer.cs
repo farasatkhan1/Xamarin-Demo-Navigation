d54f74a8013878a4a8104583b41fd76fe71c2ff4
Farasat Khan <farasat.khan@jintech.com>
2024-06-05 15:15:53 +0500

    added CustomButton with controls and custom renderer

---
+++ DemoShellNavigation/DemoShellNavigation.Android/CustomRenderer/CustomButtonRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DemoShellNavigation.Controls;
+using DemoShellNavigation.Droid.CustomRenderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace DemoShellNavigation.Droid.CustomRenderer
+{
+    class CustomButtonRenderer: Xamarin.Forms.Platform.Android.ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var button = Control;
+
+                button.SetTextColor(Android.Graphics.Color.White);
+                button.SetTypeface(null, Android.Graphics.TypefaceStyle.Bold);
+                button.SetPadding(0, 20, 0, 0);
+
+                GradientDrawable drawable = new GradientDrawable();
+                drawable.SetCornerRadius(75);
+                drawable.SetColor(Android.Graphics.Color.ParseColor("#007AFF"));
+                button.SetBackground(drawable);
+            }
+        }
+    }
+}
---
+++ DemoShellNavigation/DemoShellNavigation.iOS/CustomRenderer/CustomButtonRenderer.cs
@@ -0,0 +1,35 @@
+﻿using CoreGraphics;
+using DemoShellNavigation.Controls;
+using DemoShellNavigation.iOS.CustomRenderer;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace DemoShellNavigation.iOS.CustomRenderer
+{
+    class CustomButtonRenderer: Xamarin.Forms.Platform.iOS.ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var button = Control;
+                button.SetTitleColor(UIColor.White, UIControlState.Normal);
+                button.Font = UIFont.BoldSystemFontOfSize(button.Font.PointSize);
+                button.ContentEdgeInsets = new UIEdgeInsets(20, 0, 0, 0);
+
+                button.Layer.CornerRadius = 75;
+                button.Layer.BackgroundColor = UIColor.FromRGB(0, 122, 255).CGColor;
+                button.ClipsToBounds = true;
+            }
+        }
+    }
+}

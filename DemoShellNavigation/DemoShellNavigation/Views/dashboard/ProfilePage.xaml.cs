000c43ec3daf8762d2f285face2d9f855f34574c
Farasat Khan <farasat.khan@jintech.com>
2024-06-05 09:50:47 +0500

    added local storage preferences

--- DemoShellNavigation/DemoShellNavigation/Views/dashboard/ProfilePage.xaml.cs
+++ DemoShellNavigation/DemoShellNavigation/Views/dashboard/ProfilePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,29 @@ namespace DemoShellNavigation.Views
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            FirstnameEntry.Text = Preferences.Get("fname", "");
+            LastnameEntry.Text = Preferences.Get("lname", "");
+            EmailEntry.Text = Preferences.Get("email", "");
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e) {
+            Preferences.Set("fname", FirstnameEntry.Text);
+            Preferences.Set("lname", LastnameEntry.Text);
+            Preferences.Set("email", EmailEntry.Text);
+        }
+
+        private void OnClearPreferences(object sender, EventArgs e) {
+            Preferences.Clear();
+
+            FirstnameEntry.Text = "";
+            LastnameEntry.Text = "";
+            EmailEntry.Text = "";
         }
 
         private async void onLogoutPress(object sender, EventArgs e) {
